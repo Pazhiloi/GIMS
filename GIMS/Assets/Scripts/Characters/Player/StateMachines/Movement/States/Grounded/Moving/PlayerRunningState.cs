7c2d37a7be534a6bbfcf97202d511c6c3ac4e3cc
Miroslav Miroslavov <martinsen06@gmail.com>
2024-08-22 06:13:27 +0300

    11 Sprinting (Have bug with dashingState)

--- GIMS/Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Moving/PlayerRunningState.cs
+++ GIMS/Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Moving/PlayerRunningState.cs
@@ -8,8 +8,13 @@ namespace GIMS
 {
   public class PlayerRunningState : PlayerMovingState
   {
+
+    private PlayerSprintData sprintData;
+
+    private float startTime;
     public PlayerRunningState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
     {
+      sprintData = movementData.SprintData;
     }
 
 
@@ -18,10 +23,39 @@ namespace GIMS
     {
       base.Enter();
       stateMachine.ReusableData.MovementSpeedModifier = movementData.RunData.SpeedModifier;
+
+      startTime = Time.time;
     }
+
+    public override void Update()
+    {
+      base.Update();
+
+      if (!stateMachine.ReusableData.ShouldWalk)return;
+      if (Time.time < startTime + sprintData.RunToWalkTime) return;
+
+      StopRunning();
+
+    }
+
+
 
     #endregion
 
+    #region  Main Methods
+    private void StopRunning()
+    {
+      if (stateMachine.ReusableData.MovementInput == Vector2.zero)
+      {
+        stateMachine.ChangeState(stateMachine.IdlingState);
+
+        return;
+      }
+
+      stateMachine.ChangeState(stateMachine.WalkingState);
+    }
+
+    #endregion
 
     #region  Input Methods
     protected override void OnWalkToggleStarted(InputAction.CallbackContext context)
